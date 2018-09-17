f68c2c6301e819997ce90b1dd8eb3ff97a9175f8
nichoth <nichoth@gmail.com>
2018-09-16 20:52:01 -0700

    rpc connect in browser

--- server/index.js
+++ server/index.js
@@ -1,19 +1,36 @@
 var http = require('http')
+var S = require('pull-stream')
+var muxrpc = require('muxrpc')
 var ws = require('pull-ws/server')
+var manifest = require('../manifest.json')
 var startSSB = require('./start-ssb')
+
 
 // if we are running this as CLI, not as a module
 if (require.main === module) start()
 
 function start () {
     var server = http.createServer(function onRequest (req, res) {
-
-    }).listen(8000)
+        console.log('got request')
+    }).listen(8000, function (err) {
+        if (err) throw err
+        console.log('listening on 8000')
+    })
 
     var sbot = startSSB()
-    console.log(typeof sbot.createStream)
+    console.log(Object.keys(sbot))
 
-    ws({ server }, function (wsStream) {
+
+    ws({ server }, function onConnection (wsStream) {
+        console.log('got ws connection')
+
+        // arguments are (remote, local)
+        var rpcServer = muxrpc(null, manifest)(sbot)
+        var rpcServerStream = rpcServer.createStream(function onEnd (err) {
+            console.log('rpc stream close', err)
+        })
+
+        S(wsStream, rpcServerStream, wsStream)
     })
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,37 @@ var { h, render } = require('preact')
 var connect = require('@nichoth/preact-connect')
 var Bus = require('@nichoth/events')
 var S = require('pull-stream')
+var wsClient = require('pull-ws/client')
+var muxrpc = require('muxrpc')
 var { struct, observ } = require('./lib')
 var Effects = require('./effects')
 var View = require('./view')
 var evs = require('./EVENTS')
+var manifest = require('../manifest.json')
+var WS_URL = 'ws://localhost:8000'
+
+wsClient(WS_URL, {
+    binary: true,
+    onConnect
+})
+
+function onConnect (err, wsStream) {
+    if (err) throw err
+    console.log('got ws connection')
+
+    // sbot is rpc client
+    var sbot = muxrpc(manifest, null)()
+    var rpcStream = sbot.createStream(function onEnd (err) {
+        console.log('rpc stream close', err)
+    })
+    S(wsStream, rpcStream, wsStream)
+
+    // console.log('rpc keys', Object.keys(sbot))
+
+    sbot.whoami(function () {
+        console.log('whoami', arguments)
+    })
+}
 
 var state = struct({
     foo: observ('bar'),
