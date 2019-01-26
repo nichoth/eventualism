84bc0d3b36b8e75c8e4f83a7dca4df7bd8bf3072
nichoth <nichoth@gmail.com>
2019-01-25 21:29:14 -0800

    lint stuff

--- test/gossip.js
+++ test/gossip.js
@@ -1,16 +1,16 @@
-var Sbot = require('scuttlebot/index')
-    .use(require('scuttlebot/plugins/gossip'))
-    .use(require('ssb-blobs'))
+// var Sbot = require('scuttlebot/index')
+//     .use(require('scuttlebot/plugins/gossip'))
+//     .use(require('ssb-blobs'))
 var patchSbot = require('../server/patch-sbot')
-var ssbKeys = require('ssb-keys')
+// var ssbKeys = require('ssb-keys')
 
 module.exports = function ({ sbot, test }) {
-    var alice = ssbKeys.generate()
-    var sbot2 = patchSbot(Sbot({
-        temp: 'gossip',
-        keys: alice,
-        timeout: 1000
-    }))
+    // var alice = ssbKeys.generate()
+    // var sbot2 = patchSbot(Sbot({
+    //     temp: 'gossip',
+    //     keys: alice,
+    //     timeout: 1000
+    // }))
 
     test('gossip', function (t) {
         if (require.main === module) sbot.close()
